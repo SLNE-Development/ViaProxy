5134b42b925ca1133e60136066929ec9fc8c79ce
twisti <76837088+twisti-dev@users.noreply.github.com>
2026-08-13 23:12:37 +0200

    coordinate signed chat bridge

--- src/main/java/net/raphimc/viaproxy/proxy/packethandler/ChatSignaturePacketHandler.java
+++ src/main/java/net/raphimc/viaproxy/proxy/packethandler/ChatSignaturePacketHandler.java
@@ -6,14 +6,6 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.raphimc.viaproxy.proxy.packethandler;
 
@@ -33,6 +25,9 @@ import net.raphimc.netminecraft.constants.MCPipeline;
 import net.raphimc.netminecraft.packet.Packet;
 import net.raphimc.netminecraft.packet.PacketTypes;
 import net.raphimc.netminecraft.packet.UnknownPacket;
+import net.raphimc.viaproxy.proxy.chat.BackendChatSigningHandler;
+import net.raphimc.viaproxy.proxy.chat.ChatSigningMode;
+import net.raphimc.viaproxy.proxy.chat.ChatSigningModeResolver;
 import net.raphimc.viaproxy.proxy.session.ProxyConnection;
 
 import java.util.BitSet;
@@ -44,66 +39,93 @@ public class ChatSignaturePacketHandler extends PacketHandler {
     private final int chatSessionUpdateId;
     private final int chatMessageId;
 
-    public ChatSignaturePacketHandler(ProxyConnection proxyConnection) {
+    public ChatSignaturePacketHandler(final ProxyConnection proxyConnection) {
         super(proxyConnection);
-
         this.joinGameId = MCPackets.S2C_LOGIN.getId(proxyConnection.getClientVersion().getVersion());
         this.chatSessionUpdateId = MCPackets.C2S_CHAT_SESSION_UPDATE.getId(proxyConnection.getClientVersion().getVersion());
         this.chatMessageId = MCPackets.C2S_CHAT.getId(proxyConnection.getClientVersion().getVersion());
     }
 
     @Override
-    public boolean handleC2P(Packet packet, List<ChannelFutureListener> listeners) throws Exception {
-        if (packet instanceof UnknownPacket unknownPacket && this.proxyConnection.getC2pConnectionState() == ConnectionState.PLAY) {
-            final UserConnection user = this.proxyConnection.getUserConnection();
+    public boolean handleC2P(final Packet packet, final List<ChannelFutureListener> listeners) throws Exception {
+        if (!(packet instanceof UnknownPacket unknownPacket) || this.proxyConnection.getC2pConnectionState() != ConnectionState.PLAY) {
+            return true;
+        }
 
-            if (unknownPacket.packetId == this.chatSessionUpdateId && (!this.isP2sEncrypted() || user.has(ChatSession1_19_3.class))) {
-                return false;
-            } else if (unknownPacket.packetId == this.chatMessageId && user.has(ChatSession1_19_3.class)) {
-                final ChatSession1_19_3 chatSession = user.get(ChatSession1_19_3.class);
+        final UserConnection user = this.proxyConnection.getUserConnection();
+        final ChatSigningMode mode = ChatSigningModeResolver.resolve(this.proxyConnection);
 
-                final ByteBuf oldChatMessage = Unpooled.wrappedBuffer(unknownPacket.data);
-                final String message = PacketTypes.readString(oldChatMessage, 256); // message
-                final long timestamp = oldChatMessage.readLong(); // timestamp
-                final long salt = oldChatMessage.readLong(); // salt
+        if (mode == ChatSigningMode.PASSTHROUGH) {
+            return true;
+        }
+        if (mode == ChatSigningMode.RESIGN) {
+            return unknownPacket.packetId != this.chatSessionUpdateId;
+        }
 
-                final MessageMetadata metadata = new MessageMetadata(null, timestamp, salt);
-                final byte[] signature = chatSession.signChatMessage(metadata, message, new PlayerMessageSignature[0]);
+        if (unknownPacket.packetId == this.chatSessionUpdateId && (!this.isP2sEncrypted() || user.has(ChatSession1_19_3.class))) {
+            return false;
+        } else if (unknownPacket.packetId == this.chatMessageId && user.has(ChatSession1_19_3.class)) {
+            final ChatSession1_19_3 chatSession = user.get(ChatSession1_19_3.class);
+            final ByteBuf oldChatMessage = Unpooled.wrappedBuffer(unknownPacket.data);
+            final String message = PacketTypes.readString(oldChatMessage, 256);
+            final long timestamp = oldChatMessage.readLong();
+            final long salt = oldChatMessage.readLong();
 
-                final ByteBuf newChatMessageData = Unpooled.buffer();
-                PacketTypes.writeString(newChatMessageData, message); // message
-                newChatMessageData.writeLong(timestamp); // timestamp
-                newChatMessageData.writeLong(salt); // salt
-                Types.OPTIONAL_SIGNATURE_BYTES.write(newChatMessageData, signature); // signature
-                PacketTypes.writeVarInt(newChatMessageData, 0); // offset
-                Types.ACKNOWLEDGED_BIT_SET.write(newChatMessageData, new BitSet(20)); // acknowledged
-                if (this.proxyConnection.getClientVersion().newerThanOrEqualTo(ProtocolVersion.v1_21_5)) {
-                    newChatMessageData.writeByte(0); // checksum
-                }
-                unknownPacket.data = ByteBufUtil.getBytes(newChatMessageData);
+            final byte[] signature = chatSession.signChatMessage(
+                    new MessageMetadata(null, timestamp, salt),
+                    message,
+                    new PlayerMessageSignature[0]
+            );
+
+            final ByteBuf newChatMessageData = Unpooled.buffer();
+            PacketTypes.writeString(newChatMessageData, message);
+            newChatMessageData.writeLong(timestamp);
+            newChatMessageData.writeLong(salt);
+            Types.OPTIONAL_SIGNATURE_BYTES.write(newChatMessageData, signature);
+            PacketTypes.writeVarInt(newChatMessageData, 0);
+            Types.ACKNOWLEDGED_BIT_SET.write(newChatMessageData, new BitSet(20));
+            if (this.proxyConnection.getClientVersion().newerThanOrEqualTo(ProtocolVersion.v1_21_5)) {
+                newChatMessageData.writeByte(0);
             }
+            unknownPacket.data = ByteBufUtil.getBytes(newChatMessageData);
         }
 
         return true;
     }
 
     @Override
-    public boolean handleP2S(Packet packet, List<ChannelFutureListener> listeners) {
-        if (packet instanceof UnknownPacket unknownPacket && this.proxyConnection.getC2pConnectionState() == ConnectionState.PLAY) {
-            final UserConnection user = this.proxyConnection.getUserConnection();
+    public boolean handleP2S(final Packet packet, final List<ChannelFutureListener> listeners) {
+        if (!(packet instanceof UnknownPacket unknownPacket) || this.proxyConnection.getC2pConnectionState() != ConnectionState.PLAY) {
+            return true;
+        }
 
-            if (unknownPacket.packetId == this.joinGameId && this.isP2sEncrypted() && user.has(ChatSession1_19_3.class)) {
-                final ChatSession1_19_3 chatSession = user.get(ChatSession1_19_3.class);
-                listeners.add(f -> {
-                    if (f.isSuccess()) {
-                        final ByteBuf chatSessionUpdate = Unpooled.buffer();
-                        PacketTypes.writeVarInt(chatSessionUpdate, this.chatSessionUpdateId);
-                        PacketTypes.writeUuid(chatSessionUpdate, chatSession.getSessionId()); // session id
-                        Types.PROFILE_KEY.write(chatSessionUpdate, chatSession.getProfileKey()); // profile key
-                        this.proxyConnection.getChannel().writeAndFlush(chatSessionUpdate).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+        final UserConnection user = this.proxyConnection.getUserConnection();
+        final ChatSigningMode mode = ChatSigningModeResolver.resolve(this.proxyConnection);
+        if (mode == ChatSigningMode.PASSTHROUGH) {
+            return true;
+        }
+
+        if (unknownPacket.packetId == this.joinGameId && this.isP2sEncrypted() && user.has(ChatSession1_19_3.class)) {
+            final ChatSession1_19_3 chatSession = user.get(ChatSession1_19_3.class);
+            listeners.add(f -> {
+                if (!f.isSuccess()) {
+                    return;
+                }
+                if (mode == ChatSigningMode.RESIGN) {
+                    final BackendChatSigningHandler handler = this.proxyConnection.getChannel().pipeline().get(BackendChatSigningHandler.class);
+                    if (handler == null) {
+                        throw new IllegalStateException("Backend chat signing handler is missing from the pipeline");
                     }
-                });
-            }
+                    handler.sendSessionUpdate(chatSession);
+                    return;
+                }
+
+                final ByteBuf chatSessionUpdate = Unpooled.buffer();
+                PacketTypes.writeVarInt(chatSessionUpdate, this.chatSessionUpdateId);
+                PacketTypes.writeUuid(chatSessionUpdate, chatSession.getSessionId());
+                Types.PROFILE_KEY.write(chatSessionUpdate, chatSession.getProfileKey());
+                this.proxyConnection.getChannel().writeAndFlush(chatSessionUpdate).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+            });
         }
 
         return true;
@@ -112,5 +134,4 @@ public class ChatSignaturePacketHandler extends PacketHandler {
     private boolean isP2sEncrypted() {
         return this.proxyConnection.getChannel().attr(MCPipeline.ENCRYPTION_ATTRIBUTE_KEY).get() != null;
     }
-
 }
