6dc09753746059b3c8690978ff87e40391c135e9
twisti <76837088+twisti-dev@users.noreply.github.com>
2026-08-13 23:10:54 +0200

    extend chat signing resolver

--- src/main/java/net/raphimc/viaproxy/proxy/chat/ChatSigningModeResolver.java
+++ src/main/java/net/raphimc/viaproxy/proxy/chat/ChatSigningModeResolver.java
@@ -1,10 +1,32 @@
 package net.raphimc.viaproxy.proxy.chat;
 
+import com.viaversion.viaversion.api.minecraft.signature.storage.ChatSession1_19_3;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import net.raphimc.viaproxy.ViaProxy;
+import net.raphimc.viaproxy.proxy.session.ProxyConnection;
+
 import java.util.UUID;
 
 public final class ChatSigningModeResolver {
 
     private ChatSigningModeResolver() {
+    }
+
+    public static ChatSigningMode resolve(final ProxyConnection connection) {
+        final boolean supportedVersions = connection.getClientVersion().newerThanOrEqualTo(ProtocolVersion.v1_19_3)
+                && connection.getServerVersion().newerThanOrEqualTo(ProtocolVersion.v1_19_3);
+        final boolean hasProxyChatSession = connection.getUserConnection() != null
+                && connection.getUserConnection().has(ChatSession1_19_3.class);
+        final UUID backendProfileId = connection.getGameProfile() != null ? connection.getGameProfile().getId() : null;
+
+        return resolve(
+                supportedVersions,
+                connection.getClientVersion().equals(connection.getServerVersion()),
+                connection.getFrontendProfileId(),
+                backendProfileId,
+                ViaProxy.getConfig().shouldSignChat(),
+                hasProxyChatSession
+        );
     }
 
     public static ChatSigningMode resolve(
